de0710efe482002fa6c9bb98c59205022801ec4c
quoctruong1106 <truongb1913349@student.ctu.edu.vn>
2022-10-28 10:30:33 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        
+    <!-- <nav class="navbar ">  -->
+        <a href="/" class="navbar-brand">Ung dung Quan ly danh ba</a>
+        
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{name: 'contactbook'}" class="nav-link">
+                    Danh ba
+                    <i class="fas fa-address-book"></i>
+                </router-link>
+            </li>
+
+        </div>
+    </nav>
+</template>
