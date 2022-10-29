1641ab2dee472b19bcd591912a888f2bdf2c6509
quoctruong1106 <truongb1913349@student.ctu.edu.vn>
2022-10-29 15:40:04 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, Khong the tim thay trang. Tro ve
+            <router-link to="/">Trang Chu</router-link>
+        </p>
+    </div>
+</template>
